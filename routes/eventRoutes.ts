6bba6dbfafd902956c2ad5c1410c106f254c0e3c
Alfonso Villanueva <40200354@esmad.ipp.pt>
2023-06-09 17:59:09 +0100

    changes

--- routes/eventRoutes.ts
+++ routes/eventRoutes.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from 'express'
 import { AuthenticatedRequest } from '../models/Model'
 import { getEvents, getEventById, functionTODO, createMyEvent, getMyEvents, getMyEventById, updateMyEventById, deleteMyEventById } from '../controllers/eventController' 
+import { verifyToken } from '../controllers/authController'
 
 const router = express.Router() 
 
@@ -8,12 +9,12 @@ const router = express.Router()
 
 router.post('/myEvents', (req: Request, res: Response) => createMyEvent(req as AuthenticatedRequest, res));
 
-router.get('/myEvents', getMyEvents)
-router.get('/myEvents/:id', getMyEventById)
+router.get('/myEvents', verifyToken, getMyEvents)
+router.get('/myEvents/:id', verifyToken, getMyEventById)
 
-router.patch('/myEvents/:id', updateMyEventById)
+router.patch('/myEvents/:id', verifyToken, updateMyEventById)
 
-router.delete('/myEvents/:id', deleteMyEventById)
+router.delete('/myEvents/:id', verifyToken, deleteMyEventById)
 
 router.get('/', getEvents) 
 router.get('/:id', getEventById) 
